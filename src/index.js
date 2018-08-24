38a3b4a51c6fe74655d42112630df068f0f036f1
jacklaiu <jacklaiu@qq.com>
2018-08-24 17:58:22 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import $ from './js/jquery-vendor'
 import _ from 'lodash'
-import printMe from './print.js';
+import './style.css'
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('demo');
-    var btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
     return element
 }
 
@@ -17,6 +14,13 @@ document.body.appendChild(component());
 
 $("div.demo")
     .css('color', '#fa6565');
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     module: {
         rules: [
@@ -27,8 +31,7 @@ module.exports = {
                 /*webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader。*/
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    'style-loader', 'css-loader'
                 ]
             }
         ]
