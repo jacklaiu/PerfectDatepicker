89f301aae2df85411943ebde7287c7af54597be4
jacklaiu <jacklaiu@qq.com>
2018-08-24 14:04:37 +0800

    important commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import $ from './js/jquery-vendor'
 import _ from 'lodash'
+import printMe from './print.js';
 
 function component() {
-
     var element = document.createElement('div');
-
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    element.classList.add('demo')
-
+    element.classList.add('demo');
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element
 }
 
 document.body.appendChild(component());
 
-$("div.demo").css('font-weight', 'bold');
+$("div.demo")
+    .css('color', '#fa6565');
 
 
 
